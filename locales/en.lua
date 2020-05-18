1176478cd26fd64e70a68a43695a9c354781a742
Lukas Fülling <lukas@k40s.net>
2020-05-18 21:48:47 +0200

    move translations to correct file

--- locales/en.lua
+++ locales/en.lua
@@ -1,6 +1,6 @@
 Locales['en'] = {
-  ['respawn_available_in'] = 'Terugkeren in ~b~%s minuten %s seconden~s~',
-  ['respawn_prompt'] = 'Houdt [~b~E~s~] ingedrukt om terug te keren',
-  ['wasted'] = 'Overleden',
+    ['respawn_available_in'] = 'respawn available in ~b~%s minutes %s seconds~s~',
+    ['respawn_prompt'] = 'hold [~b~E~s~] to respawn',
+    ['wasted'] = 'Wasted',
 }
 
---
+++ locales/nl.lua
@@ -0,0 +1,6 @@
+Locales['nl'] = {
+  ['respawn_available_in'] = 'Terugkeren in ~b~%s minuten %s seconden~s~',
+  ['respawn_prompt'] = 'Houdt [~b~E~s~] ingedrukt om terug te keren',
+  ['wasted'] = 'Overleden',
+}
+
