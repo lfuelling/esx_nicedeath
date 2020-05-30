37664c707690b8963fc6d61bac0839c81a760f9a
Gabriel Calderon <835733+gabrielcalderon@users.noreply.github.com>
2020-05-30 15:57:40 -0400

    Create es.lua

---
+++ locales/es.lua
@@ -0,0 +1,5 @@
+Locales['es'] = {
+    ['respawn_available_in'] = 'Reaparecer estara disponible en ~b~%s minutos %s segundos~s~',
+    ['respawn_prompt'] = 'Presione [~b~E~s~] para reaparecer',
+    ['wasted'] = 'Fallecido',
+}
